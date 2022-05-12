fde9966dd3ab2ccc9c5df9a3155b57c7c15d0c16
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-12 17:44:27 -0300

    Creating finder

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,15 +1,24 @@
 import styles from './Menu.module.scss'
 import { ReactComponent as Logo} from 'assets/logo.svg'
+import Finder from './Finder'
+import { useState } from 'react'
 
 export default function Menu() {
+
+    const [search, setSearch] = useState<string>('')
+
     return (
         <main>
-            <nav className={styles.menu}>
+            <nav className={styles.menuBar}>
                 <Logo/>
             </nav>
             <header className={styles.header}>
                 <div className={styles.header__text}>A casa do código e da massa</div>
             </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Cardápío</h3>
+                <Finder search={search} setSearch={setSearch} />
+            </section>
         </main>
     )
 }
