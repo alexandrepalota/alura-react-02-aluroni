e0086cd3eea52bd571d674da4a2cfa6ae5270045
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-17 18:02:07 -0300

    Not Found page, useNavigate hook

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,23 @@ import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import DefaultLayout from 'components/DefaultLayout';
 import About from 'pages/About';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <BrowserRouter>
         <NavMenu />
         <Routes>
           <Route path='/' element={<DefaultLayout />}>
-            <Route index element={<Home/>}/>
-            <Route path='cardapio' element={<Menu/>}/>
-            <Route path='sobre' element={<About/>}/>
+            <Route index element={<Home />} />
+            <Route path='cardapio' element={<Menu />} />
+            <Route path='sobre' element={<About />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </main>
   );
