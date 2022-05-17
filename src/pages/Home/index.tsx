548bfed457701730a9553ccd9c8f6c4f545ffc4a
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-17 17:00:00 -0300

    About page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import menu from 'data/menu.json';
 import stylesTheme from 'styles/Theme.module.scss';
 import styles from './Home.module.scss';
+import ourHome from 'assets/nossa_casa.png';
 
 export default function Home() {
   let recommendedDishes = [...menu];
@@ -19,6 +20,13 @@ export default function Home() {
             <button className={styles.recommended__button}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTheme.title}>Nossa casa</h3>
+      <div className={styles.ourHome}>
+        <img src={ourHome} alt="Casa do aluroni" />
+        <div className={styles.ourHome__address}>
+            Rua Vergueiro, 3185 <br/><br/> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,18 @@ import NavMenu from 'components/NavMenu';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import DefaultLayout from 'components/DefaultLayout';
+import About from 'pages/About';
 
 export default function AppRouter() {
   return (
     <main>
       <BrowserRouter>
         <NavMenu />
-        
         <Routes>
           <Route path='/' element={<DefaultLayout />}>
             <Route index element={<Home/>}/>
             <Route path='cardapio' element={<Menu/>}/>
+            <Route path='sobre' element={<About/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
